0a0e861347ac223cc68121a3304f59341f19cfe0
Yury Lebedev <yury.lebedev@daimler.com>
2018-02-26 12:15:46 +0100

    Add fzf user functions to fish shell

--- dotfiles/fish/user_functions.fish
+++ dotfiles/fish/user_functions.fish
@@ -2,3 +2,19 @@ function mkcd
   mkdir $argv
   cd $argv
 end
+
+function fzfcheckout -d "Fuzzy-find and checkout a git branch"
+  git branch --all | grep -v HEAD | string trim | fzf | xargs git checkout
+end
+
+function fhistory -d "Fuzzy-find history repeat"
+  eval (history | fzf)
+end
+
+function fkill -d "Fuzzy-find and kill process"
+  ps -ef | sed 1d | fzf -m | awk '{print $2}' | xargs kill
+end
+
+function fnvim -d "Fuzzy-find file and open in nvim"
+  fzf | xargs nvim
+end
