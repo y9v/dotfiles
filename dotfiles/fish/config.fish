4d4b31ba7b9cad5d4d0f94d92a0bc3d7d7829008
Yury Lebedev <lebedev.yurii@gmail.com>
2020-07-07 14:17:12 +0200

    Fix fish config

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -2,8 +2,8 @@ source ~/.config/fish/aliases.fish
 source ~/.config/fish/variables.fish
 source ~/.config/fish/user_functions.fish
 
-set -Ux fish_greeting ""
-set -Ux theme_color_scheme gruvbox
+set fish_greeting ""
+set theme_color_scheme gruvbox
 
 function fish_mode_prompt; end
 function fish_vi_cursor; end
@@ -17,7 +17,5 @@ export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
 
 export BAT_STYLE=numbers,changes
 export BAT_THEME=1337
-
-set -Ux fish_user_paths "/usr/local/opt/nss/bin" $fish_user_paths
 
 source /usr/local/opt/asdf/asdf.fish
--- dotfiles/fish/variables.fish
+++ dotfiles/fish/variables.fish
@@ -1,7 +1,6 @@
 set -Ux HIST_STAMPS "dd.mm.yyyy"
 
-set -Ux PATH $HOME /usr/local/bin /usr/bin /bin /usr/sbin /sbin $PATH
-set -Ux fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
 set -Ux EDITOR vim
 set -Ux BUNDLER_EDITOR vim
@@ -9,5 +8,5 @@ set -Ux BUNDLER_EDITOR vim
 set -Ux LC_ALL en_US.UTF-8
 set -Ux LANG en_US.UTF-8
 
-set -Ux fish_user_paths "/usr/local/opt/gnu-getopt/bin:/usr/local/opt/openssl/bin" $fish_user_paths
-set -Ux fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/gnu-getopt/bin" $fish_user_pa
+set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
